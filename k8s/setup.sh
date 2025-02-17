f11369e4f8eb9538a1de2509c187e1895d026e63
Aaron Elkiss <aelkiss@umich.edu>
2025-02-17 14:06:12 -0500

    Update setup.sh to reference token api

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -18,11 +18,15 @@ fi
 
 if [[ ! -e $path/token ]];
 then
-  echo "Put token (from service account secret) in $path/token"
+  echo "A user with appropriate permissions needs to run:" 
+  echo "kubectl -n ingest create token ingest-prep --duration=REASONABLY_LARGE_NUMBER_OF_SECONDs"
+  echo "Put the token (from service account secret) in $path/token"
   exit 1;
 fi
 
 $path/kubectl config set-cluster cluster --certificate-authority $path/k8s-ca.crt --server $KUBERNETES_SERVER
-$path/kubectl config set-credentials default --token $(cat $path/token)
-$path/kubectl config set-context default --cluster cluster --user default --namespace ingest
+$path/kubectl config set-credentials ingest-prep --token $(cat $path/token)
+$path/kubectl config set-context default --cluster cluster --user ingest-prep --namespace ingest
 $path/kubectl config use-context default
+
+rm $path/token
